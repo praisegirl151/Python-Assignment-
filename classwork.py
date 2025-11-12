eaa1d646acb815e979336eaf7335c16037e53725
Praise Otuka <praiseotuka18@gmail.com>
2025-11-12 10:24:10 +0100

    Added Classwork and another assignment

---
+++ classwork.py
@@ -0,0 +1,54 @@
+#Classwork
+
+#Testing Escape Characters
+
+wr = "I will send down an \'Army to find you "
+print(wr)
+
+wr = "I wake up as early as 5am  True\\False"
+print(wr)
+
+p = "Arise and Shine \nits a new morning "
+print(p)
+
+p = "Life is a Journey \rdecide how you want to travel By Flight\\ By Road"
+print(p)
+
+p = "Life is a Journey \t live it what way you can afford"
+print(p)
+
+p = "Awake,\bthe money is finished"
+print(p)
+
+p = "we are good \f to go"
+print(p)
+
+#String Methods
+N = "awake"
+txt =N.capitalize()
+print(txt)
+
+A = "AWAKE"
+txt =A.casefold()
+print(txt)
+
+b = "Love is a beautiful thing"
+txt = b.center(60)
+print(txt)
+
+c = "Today is a good day, today is going as we planned it "
+txt = c.count("is")
+print(txt)
+
+d = "My name is Praise"
+txt = d.encode()
+print(txt)
+
+e = "Now its Good."
+txt = e.endswith(".")
+print(txt)
+
+f = ("H\te\tl\tl\to")
+txt = f.expandtabs("")
+print(txt)
+
